387bb3225c3908b69c3b0df317d3c72a08a8cf37
James Kuban <kubandj@email.arizona.edu>
2021-04-12 19:46:43 -0700

    Update droneos.cpp

--- droneos.cpp
+++ droneos.cpp
@@ -18,12 +18,38 @@
 #include "console.h"
 
 //TODO: Add Tasks
-static void 
+static void proximitySensors(void *pvParameters);
+static void imu(void *pvParameters);
+static void gps(void *pvParameters);
+//static void gpsMon(void *pvParameters);
+static void videoFeed(void *pvParameters);
+static void videoForward(void *pvParameters);
+//static void videoMon(void *pvParameters)
+//static void proxMon(void *pvParameters);//TODO: Something
+static void control(void *pvParameters);
+//static void imuMon(void *pvParameters);//TODO: Something
+static void motor(void *pvParameters);
+//static void motorMon(void *pvParameters);
+static void monitor(void *pvParameters);
 
 //TODO: Add Queues
-static QueueHandle_t
+static QueueHandle_t proxRx;
+static QueueHandle_t imuRx;
+static QueueHandle_t gpsRx;
+static QueueHandle_t gpsTx;
+static QueueHandle_t videoRx;
+static QueueHandle_t videoTx;
+static QueueHandle_t commandRx;
+static QueueHandle_t commandTx;
+static QueueHandle_t motorTx;
 
 void main_drone(void)
 {
     
+    
+    
+    if(){
+        
+        xTaskCreate(monitor, "MON", configMINIMAL_STACK_SIZE, NULL, MONITOR_TASK_PRIORITY, NULL);
+    }
 }
